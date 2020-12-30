1468e39811f66837d4320d5b5f41e7ac9dafe994
unknown <ngunjiri.kevin@yahoo.com>
2020-12-30 09:49:29 -0500

    add CampsiteInforComponent

---
+++ React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class CampsiteInfo extends Component {
+    constructor (props) {
+        super(props)
+    }
+
+    renderCampsite(campsite){
+        
+            if (campsite){
+                return(
+                    <div className= "col-md-5 m-1">
+                        <Card>
+                            <CardImg top src = {campsite.image} alt= {campsite.name} />
+                            <CardBody>
+                                <CardTitle>{campsite.name}</CardTitle>
+                                <CardText>{campsite.description}</CardText>
+                            </CardBody> 
+                        </Card>
+                    </div>
+                )
+                    
+            }
+            return <div />
+    }
+    
+    render() {
+        return (
+            <div className = "col">
+                {this.renderCampsite(this.props.campsite)};
+            </div>
+        )
+    }
+}
+
+export default CampsiteInfo;
